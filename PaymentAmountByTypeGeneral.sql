31e3add93742749ce480a4e8d1a64a19df3cba3f
yuvarani <yuvaranikv@gmail.com>
2023-02-07 16:59:32 +0530

    added sql files

---
+++ PaymentAmountByTypeGeneral.sql
@@ -0,0 +1,4 @@
+select sum(Total_Amount_of_Payment_USDollars)  from OP_DTL_GNRL_PGYR2021_P01202023;
+
+
+ 
---
+++ PaymentAmountByTypeResearch.sql
@@ -0,0 +1,1 @@
+select sum(Total_Amount_of_Payment_USDollars) from OP_DTL_RSRCH_PGYR2021_P01202023
